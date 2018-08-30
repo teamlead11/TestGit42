2614ffdeef493fc117c264dfabcc9df61e683a33
vicz95 <mail2vignesh95@gmail.com>
2018-08-30 14:06:08 +0530

    Palindrome has been updated by sathish

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -7,6 +7,7 @@ public class Framework {
 		System.out.println("this is my framework");
 		System.out.println("pavithra add a method");
 		stringPanlindromeByNisha();
+		stringPalindromeBySathish("Sathish");
 	}
 
 	public static void StringPalindromeByGanesh() {
@@ -16,13 +17,29 @@ public class Framework {
 
 	}
 	
-public static void stringPanlindromeByNisha() {
-	String s="nisha";
-	String reverse="";
-	for(int i=0;i<s.length();i++) {
-		reverse=reverse+s.charAt(i);
-		
+	public static void stringPanlindromeByNisha() {
+		String s="nisha";
+		String reverse="";
+		for(int i=0;i<s.length();i++) {
+			reverse=reverse+s.charAt(i);
+			
+		}
+		System.out.println(reverse);
 	}
-	System.out.println(reverse);
-}
+	
+	public static void stringPalindromeBySathish(String a) {
+		String b= "";
+        for(int i = a.length() - 1; i >= 0; i--)
+        {
+            b = b + a.charAt(i);
+        }
+        if(a.equalsIgnoreCase(b))
+        {
+            System.out.println(a + " - This string is palindrome.");
+        }
+        else
+        {
+            System.out.println(a + " - This string is not palindrome.");
+        }
+	}
 }
