0a8d665a082666cf822ee492e98954bbe2aacf20
madhuri <maduri0212@gmail.com>
2018-08-30 15:15:56 +0530

    madhuri added string palindrome program

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -1,5 +1,7 @@
 package testpack;
 
+import java.util.Scanner;
+
 public class Framework {
 
 	public static void main(String[] args) {
@@ -8,38 +10,55 @@ public class Framework {
 		System.out.println("pavithra add a method");
 		stringPanlindromeByNisha();
 		stringPalindromeBySathish("Sathish");
+		stringPalindromeByMadhuri();
 	}
 
 	public static void StringPalindromeByGanesh() {
 
 	}
+
 	public static void StringPalindromeBypavithra() {
 
 	}
-	
+
 	public static void stringPanlindromeByNisha() {
-		String s="nisha";
-		String reverse="";
-		for(int i=0;i<s.length();i++) {
-			reverse=reverse+s.charAt(i);
-			
+		String s = "nisha";
+		String reverse = "";
+		for (int i = 0; i < s.length(); i++) {
+			reverse = reverse + s.charAt(i);
+
 		}
 		System.out.println(reverse);
 	}
-	
+
 	public static void stringPalindromeBySathish(String a) {
-		String b= "";
-        for(int i = a.length() - 1; i >= 0; i--)
-        {
-            b = b + a.charAt(i);
-        }
-        if(a.equalsIgnoreCase(b))
-        {
-            System.out.println(a + " - This string is palindrome.");
-        }
-        else
-        {
-            System.out.println(a + " - This string is not palindrome.");
-        }
+		String b = "";
+		for (int i = a.length() - 1; i >= 0; i--) {
+			b = b + a.charAt(i);
+		}
+		if (a.equalsIgnoreCase(b)) {
+			System.out.println(a + " - This string is palindrome.");
+		} else {
+			System.out.println(a + " - This string is not palindrome.");
+		}
 	}
-}
+
+	public static void stringPalindromeByMadhuri() {
+			 String str, rev = "";
+		      Scanner sc = new Scanner(System.in);
+		 
+		      System.out.println("Enter a string:");
+		      str = sc.nextLine();
+		 
+		      int length = str.length();
+		 
+		      for ( int i = length - 1; i >= 0; i-- )
+		         rev = rev + str.charAt(i);
+		 
+		      if (str.equals(rev))
+		         System.out.println(str+" is a palindrome");
+		      else
+		         System.out.println(str+" is not a palindrome");		 
+		   }
+		
+	}
