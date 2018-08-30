3085934aecd06ca769ac1960f6fd78955deb0a1e
teamlead11 <automationlead11@gmail.com>
2018-08-30 09:55:12 +0530

    added framework

---
+++ src/testpack/Framework.java
@@ -0,0 +1,14 @@
+package testpack;
+
+public class Framework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my framework");
+	}
+
+	public static void StringPalindromeByGanesh() {
+
+	}
+
+}
