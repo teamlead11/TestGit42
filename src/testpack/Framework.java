d40c7a2d53f68c93b90bbfcf2d354488cc193ef0
pavi261991 <pavirao26@gmail.com>
2018-08-29 23:01:17 -0700

    add a method in framework

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -5,10 +5,15 @@ public class Framework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my framework");
+		System.out.println("pavithra add a method");
 	}
 
 	public static void StringPalindromeByGanesh() {
 
 	}
+	public static void StringPalindromeBypavithra() {
+
+	}
+	
 
 }
