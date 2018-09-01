570f0942992db2fb6fea369bdd22f9a944bbc39c
karthikachandran046 <karthikachandran046>
2018-09-01 11:46:40 +0530

    added reverse program

--- src/testpack/Framework.java
+++ src/testpack/Framework.java
@@ -3,33 +3,25 @@ package testpack;
 import java.util.Scanner;
 
 public class Framework {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("this is my framework");
 		System.out.println("pavithra add a method");
 		stringPanlindromeByNisha();
 		stringPalindromeBySathish("Sathish");
 		stringPalindromeBySelvamani();
 	}
-
-	public static void StringPalindromeByGanesh() {
-
-	}
+  public static void StringPalindromeByGanesh() {
+}
 	public static void StringPalindromeBypavithra() {
-
-	}
-	
+}
 	public static void stringPanlindromeByNisha() {
 		String s="nisha";
 		String reverse="";
 		for(int i=0;i<s.length();i++) {
 			reverse=reverse+s.charAt(i);
-			
-		}
+			}
 		System.out.println(reverse);
 	}
-	
 	public static void stringPalindromeBySathish(String a) {
 		String b= "";
         for(int i = a.length() - 1; i >= 0; i--)
@@ -45,8 +37,7 @@ public class Framework {
             System.out.println(a + " - This string is not palindrome.");
         }
 	}
-
-	public static void stringPalindromeBySelvamani() {
+   public static void stringPalindromeBySelvamani() {
 		String original, reverse ="";
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the value to check Palindrome");
@@ -63,4 +54,12 @@ public class Framework {
 			System.out.println(original + " is not a palindrome");
 		}
 }
+   public static void stringPanlindromeBykarthika() {
+		String s="karthika";
+		String rev="";
+		for(int i=0;i<s.length();i++) {
+			rev=rev+s.charAt(i);
+			}
+		System.out.println(rev);
+	}
 }
